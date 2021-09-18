a57cc848ee13a7fb50cd292bceefcae9c8560513
eunseo2 <20180972@sungshin.ac.kr>
2021-09-18 11:07:37 +0900

    [feat] 초기 세팅

---
+++ setgame/src/test/java/com/prgrms/setgame/SetgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.setgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
