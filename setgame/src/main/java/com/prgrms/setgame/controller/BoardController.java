0f2938fbc05ea5d05efa282abfc161cdef8ebc70
eunseo2 <20180972@sungshin.ac.kr>
2021-09-23 17:43:45 +0900

    [feat:#5] +3 cards 화면 구현

--- setgame/src/main/java/com/prgrms/setgame/controller/BoardController.java
+++ setgame/src/main/java/com/prgrms/setgame/controller/BoardController.java
@@ -1,11 +1,14 @@
 package com.prgrms.setgame.controller;
 
+import com.prgrms.setgame.error.Message;
 import com.prgrms.setgame.model.Board;
 import com.prgrms.setgame.service.BoardService;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,12 +21,36 @@ public class BoardController {
         this.boardService = boardService;
     }
 
-    @GetMapping("/boards")
-    public String boardsPage(Model model) {
+    @GetMapping("/game")
+    public String boardsMainPage(Model model) {
         List<Board> boards = boardService.boards().stream().collect(Collectors.toList());
         Collections.shuffle(boards);
 
         model.addAttribute("boards", boards);
         return "main";
+    }
+
+    @GetMapping("/boards")
+    public String boardsPage(Model model) {
+        List<Board> boards = boardService.findAll().stream().collect(Collectors.toList());
+        Collections.shuffle(boards);
+
+        model.addAttribute("boards", boards);
+        return "main";
+    }
+
+    @GetMapping("/boards/add")
+    public String boardsNewPage(Model model) throws NotFoundException {
+        List<Board> boards = new ArrayList<>();
+
+        if (boardService.findAll().stream().count() < 15) {
+            boards = boardService.addBoards().stream().collect(Collectors.toList());
+            Collections.shuffle(boards);
+            model.addAttribute("boards", boards);
+            return "main";
+        }
+
+        model.addAttribute("message", Message.CANNOT_FIND_CARDS);
+        return "message";
     }
 }
